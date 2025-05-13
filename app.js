66e65b52b717a46290820fd2a7dcb3c5e9fff97b
FrankGrillo1991 <fsg12491@aol.com>
2025-05-12 21:11:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("form");
-const email = document.getElementById("input");
+const email = document.getElementById("email");
 const error = document.getElementById("error");
 
 form.addEventListener("submit", (e) => {
