182fb572043a8cc40444792ae7f74fad79f7439a
FrankGrillo1991 <fsg12491@aol.com>
2025-05-12 21:31:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-
+const email = document.getElementById("email").value;
+const emailError = document.getElementById("email-error");
+const successMessage = document.getElementById("success-message");
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
 function validateEmail() {
-    const email = document.getElementById("email").value;
-  const emailError = document.getElementById("email-error");
-  const successMessage = document.getElementById("success-message");
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
     if (!emailPattern.test(email)) {
-    emailError.textContent = "Please enter a valid email address.";
-    successMessage.textContent = ""; // clear success
-    return false; // prevent form submission
-  } else {
-    emailError.textContent = "";
-    successMessage.textContent = "Email submitted successfully!";
-    return false; // just show message, don't reload page
-  }
+        emailError.textContent = "Please enter a valid email address.";
+        successMessage.textContent = ""; // clear success
+        return false; // prevent form submission
+    } else {
+        emailError.textContent = "";
+        successMessage.textContent = "Email submitted successfully!";
+        return false; // just show message, don't reload page
+    }
 }
 
